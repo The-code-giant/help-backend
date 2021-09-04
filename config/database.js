ab3b90e3189bc0479ec8fa8ace7b96671cd1bc4e
Salman Samian <salman.samian@gmail.com>
2021-09-04 18:28:09 +0430

    update +contactus apis

--- config/database.js
+++ config/database.js
@@ -1,26 +1,20 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: false
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'parsalpha-ir'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
-  };
-};
+  },
+});
